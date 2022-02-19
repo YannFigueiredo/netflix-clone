a328d2a7e20334f62465ab8f5a52e47b6bf53af7
Yann Figueiredo <yann.fabricio@hotmail.com>
2022-02-18 21:46:41 -0300

    Corrigido erro de exibição de nomes das séries

--- src/components/ListaMidias/index.js
+++ src/components/ListaMidias/index.js
@@ -1,15 +1,30 @@
 import './lista-midias.css';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import { useEffect, useRef } from 'react';
 
-export default function listaMidias({ lista }){
+export default function ListaMidias({ lista }){
+    const areaMidia = useRef(null);
+    const btnVoltar = useRef(null);
+    const btnAvancar = useRef(null);
+
+    useEffect(() => {
+        function eventoScroll(){
+            areaMidia.current.addEventListener('scroll', () => {
+                console.log('olaaa');
+             })
+        }
+        
+        eventoScroll();
+    }, []);
+
     return(
         <section className="lista-midias">
             <h1 className="lista-midias--titulo">{lista.titulo}</h1>
-            <div className="lista-midias--area-midia">
-                <div className="area-btn-slide area-btn-voltar">
+            <div className="lista-midias--area-midia" ref={areaMidia}>
+                <div className="area-btn-slide area-btn-voltar" ref={btnVoltar}>
                     <IoIosArrowBack className="btn-voltar"/>
                 </div>
-                <div className="area-btn-slide area-btn-avancar">
+                <div className="area-btn-slide area-btn-avancar" ref={btnAvancar}>
                     <IoIosArrowForward className="btn-avancar"/>
                 </div>
                 {   
@@ -19,7 +34,7 @@ export default function listaMidias({ lista }){
                                 <img src={'http://image.tmdb.org/t/p/w300'+item.poster_path} alt={item.title}/>
                             </div>
                             <div className="midia--detalhes">
-                                <h2 className="midia--titulo">{item.title}</h2>
+                                <h2 className="midia--titulo">{typeof(item.title) !== 'string' ? item.name : item.title}</h2>
                             </div>
                         </article>
                     ))
