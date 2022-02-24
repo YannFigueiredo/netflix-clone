c077cffe6f1eb72719773673ac3b668a83ca30ee
Yann Figueiredo <yann.fabricio@hotmail.com>
2022-02-23 21:49:29 -0300

    Ajustada exibição de botoões controladores dos slides

--- src/components/ListaMidias/index.js
+++ src/components/ListaMidias/index.js
@@ -9,40 +9,40 @@ export default function ListaMidias({ lista }){
 
     const avancarSlide = (e) => {
         e.preventDefault();
-        //btnAvancar.current.style.position = 'absolute';
-        //btnAvancar.current.style.right = '0';
+
         areaMidia.current.scrollLeft += areaMidia.current.offsetWidth/2;
     };
 
     const voltarSlide = (e) => {
         e.preventDefault();
-        //btnAvancar.current.style.position = 'absolute';
-        //btnAvancar.current.style.right = '0';
+
         areaMidia.current.scrollLeft -= areaMidia.current.offsetWidth/2;
     };
 
     return(
         <section className="lista-midias">
             <h1 className="lista-midias--titulo">{lista.titulo}</h1>
-            <div className="lista-midias--area-midia" ref={areaMidia}>
-                <div className="area-btn-slide area-btn-voltar" ref={btnVoltar} onClick={voltarSlide}>
-                    <IoIosArrowBack className="btn-voltar"/>
+            <div className='container-auxiliar'>
+                <div className="lista-midias--area-midia" ref={areaMidia}>
+                    <div className="area-btn-slide area-btn-voltar" ref={btnVoltar} onClick={voltarSlide}>
+                        <IoIosArrowBack className="btn-voltar"/>
+                    </div>
+                    <div className="area-btn-slide area-btn-avancar" ref={btnAvancar} onClick={avancarSlide}>
+                        <IoIosArrowForward className="btn-avancar"/>
+                    </div>
+                    {   
+                        lista.itens.map((item, key) => (
+                            <article key={key} className="lista-midias--midia">
+                                <div className="midia--capa">
+                                    <img src={'http://image.tmdb.org/t/p/w300'+item.poster_path} alt={item.title}/>
+                                </div>
+                                <div className="midia--detalhes">
+                                    <h2 className="midia--titulo">{typeof(item.title) !== 'string' ? item.name : item.title}</h2>
+                                </div>
+                            </article>
+                        ))
+                    }
                 </div>
-                <div className="area-btn-slide area-btn-avancar" ref={btnAvancar} onClick={avancarSlide}>
-                    <IoIosArrowForward className="btn-avancar"/>
-                </div>
-                {   
-                    lista.itens.map((item, key) => (
-                        <article key={key} className="lista-midias--midia">
-                            <div className="midia--capa">
-                                <img src={'http://image.tmdb.org/t/p/w300'+item.poster_path} alt={item.title}/>
-                            </div>
-                            <div className="midia--detalhes">
-                                <h2 className="midia--titulo">{typeof(item.title) !== 'string' ? item.name : item.title}</h2>
-                            </div>
-                        </article>
-                    ))
-                }
             </div>
         </section>
     );
